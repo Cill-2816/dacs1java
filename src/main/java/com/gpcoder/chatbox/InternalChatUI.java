262eacc137bb1635b50ef7dfef119d6ccc9d9eac
ducanhvtt01 <ducanhvtt01@gmail.com>
2025-05-14 16:10:48 +0700

    hi

--- src/main/java/com/gpcoder/chatbox/InternalChatUI.java
+++ src/main/java/com/gpcoder/chatbox/InternalChatUI.java
@@ -318,6 +318,7 @@ public class InternalChatUI extends JFrame {
 
     private void runNetworking() {
     try {
+        // this.socket = new Socket("localhost", 12345);
         this.socket = new Socket("26.106.134.18", 12345);
         this.outStream = new ObjectOutputStream(socket.getOutputStream());
         this.inStream = new ObjectInputStream(socket.getInputStream());
